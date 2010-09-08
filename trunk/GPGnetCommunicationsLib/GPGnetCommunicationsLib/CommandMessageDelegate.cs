49f08101f97cff226995119cf761b0c54359ae85
fwnexusone <fwnexusone@38cc2beb-df07-7e2c-a4b0-91b77d6e01a2>
2010-09-08 09:12:58 +0000

    - Used wrong exporter

--- trunk/GPGnetCommunicationsLib/GPGnetCommunicationsLib/CommandMessageDelegate.cs
+++
@@ -1,8 +0,0 @@
-namespace GPGnetCommunicationsLib
-{
-    using System;
-    using System.Runtime.CompilerServices;
-
-    public delegate void CommandMessageDelegate(int connectionID, CommandMessage message);
-}
-
