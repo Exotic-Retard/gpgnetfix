d46f082a8d3ef997ab0e4c65381e0220072611aa
fwnexusone <fwnexusone@38cc2beb-df07-7e2c-a4b0-91b77d6e01a2>
2010-09-08 10:44:37 +0000

    - Added GPG.Multiplayer.Statistics

---
+++ trunk/GPG.Multiplayer.Statistics/AssemblyInfo.vb
@@ -0,0 +1,17 @@
+﻿' Assembly GPG.Multiplayer.Statistics, Version 1.0.0.0
+
+<Assembly: System.Reflection.AssemblyVersion("1.0.0.0")>
+<Assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows:=True)>
+<Assembly: System.Reflection.AssemblyTrademark("")>
+<Assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)>
+<Assembly: System.Reflection.AssemblyProduct("GPG.Multiplayer.Statistics")>
+<Assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)>
+<Assembly: System.Runtime.InteropServices.Guid("43495278-6c24-4cd0-9250-9793afab9d55")>
+<Assembly: System.Runtime.InteropServices.ComVisible(False)>
+<Assembly: System.Reflection.AssemblyCopyright("Copyright " & ChrW(169) & "  2007 Gas Powered Games")>
+<Assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")>
+<Assembly: System.Reflection.AssemblyCompany("Gas Powered Games")>
+<Assembly: System.Reflection.AssemblyConfiguration("")>
+<Assembly: System.Reflection.AssemblyDescription("")>
+<Assembly: System.Reflection.AssemblyTitle("GPG.Multiplayer.Statistics")>
+
---
+++ trunk/GPG.Multiplayer.Statistics/GPG/Multiplayer/Statistics/Properties/Resources.vb
@@ -0,0 +1,45 @@
+﻿Imports System
+Imports System.CodeDom.Compiler
+Imports System.ComponentModel
+Imports System.Diagnostics
+Imports System.Globalization
+Imports System.Resources
+Imports System.Runtime.CompilerServices
+
+Namespace GPG.Multiplayer.Statistics.Properties
+    <GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "2.0.0.0"), DebuggerNonUserCode, CompilerGenerated> _
+    Friend Class Resources
+        ' Methods
+        Friend Sub New()
+        End Sub
+
+
+        ' Properties
+        <EditorBrowsable(EditorBrowsableState.Advanced)> _
+        Friend Shared Property Culture As CultureInfo
+            Get
+                Return Resources.resourceCulture
+            End Get
+            Set(ByVal value As CultureInfo)
+                Resources.resourceCulture = value
+            End Set
+        End Property
+
+        <EditorBrowsable(EditorBrowsableState.Advanced)> _
+        Friend Shared ReadOnly Property ResourceManager As ResourceManager
+            Get
+                If (Resources.resourceMan Is Nothing) Then
+                    Dim manager As New ResourceManager("GPG.Multiplayer.Statistics.Properties.Resources", GetType(Resources).Assembly)
+                    Resources.resourceMan = manager
+                End If
+                Return Resources.resourceMan
+            End Get
+        End Property
+
+
+        ' Fields
+        Private Shared resourceCulture As CultureInfo
+        Private Shared resourceMan As ResourceManager
+    End Class
+End Namespace
+
